2344681f051fb4717cb5c98552a069e6af6b769c
PRANAV8966 <pranav.wan706@gmail.com>
2025-05-09 17:13:06 +0530

    added route

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 const app = express();
 
+
+import { StatusCodes } from 'http-status-codes';
 import { PORT } from './src/config/server-config.js';
 import { connect } from './src/config/db-config.js';
 import apiRoutes from './src/routes/index.js';
@@ -11,7 +13,13 @@ const startServer = async () => {
     app.use(express.json());
 
     await connect();
-
+    app.get('/', (req, res) => {
+        return res.status(StatusCodes.OK).json({
+            success:true,
+            message:'API working properly',
+            error:{}
+        });
+    })
     app.use('/api', apiRoutes);
     app.listen(PORT, () => {
         console.log(`server started on ${PORT}`);
